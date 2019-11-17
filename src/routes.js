030d49a9c5e4b95f9fa26dec302e80364e03b941
Matheus Leite <matheusjoselfm@gmail.com>
2019-11-17 20:27:27 -0300

    Reajustando tamanho das imagens e adicionando like

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,15 @@ const express = require('express');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
 const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
 
 const routes = new express.Router();
 const upload = multer(uploadConfig);
 
-
+//Posts
 routes.get('/posts', PostController.index); 
 routes.post('/posts', upload.single('image'), PostController.store); 
+//Posts - Likes
+routes.post('/posts/:id/like', LikeController.store); 
 
 module.exports = routes;
