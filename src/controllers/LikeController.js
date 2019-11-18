40beaf421c88f4567c33c470ff1ec95e1276b82d
Matheus Leite <matheusjoselfm@gmail.com>
2019-11-17 21:07:31 -0300

    add socket io, ajuste nas imagens e outras configuracoes

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -6,6 +6,8 @@ module.exports = {
         post.likes +=1 ;
         await post.save();
 
+        req.io.emit('like', post);
+
         return res.json(post)
     }
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
 
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect("mongodb+srv://api-instagram:api-instagram@cluster0-6b2bu.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true, // Configuracao para identificar o formato da conexao nova a partir de uma String.
     useUnifiedTopology: true
 });
 
+app.use((req, resp, next) => {
+    req.io = io;
+
+    next(); // Garantirá que esse middleware seja executado, porem os outros middlewares executados depois
+            // tambem sejam executados
+});
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
 app.use(require('./routes'));
 
-app.listen(8080);
+server.listen(8080);
