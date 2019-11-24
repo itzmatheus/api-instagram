c6058fa18e11aba9b46539d6c92b9d60530efafc
Matheus Leite <matheusjoselfm@gmail.com>
2019-11-24 14:43:50 -0300

    Adicionando configuracao do S3 Storage

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -12,19 +12,25 @@ module.exports = {
     async store(req, res) {
 
         const { author, place, description, hashtags } = req.body;
-        const { filename: image } = req.file;  
+        const { filename: image, key, location: url = "" } = req.file;  
         
-        const [name] = image.split('.');
-        const fileName = `${name}.jpg`;
+        let fileName = url;
 
-        await sharp(req.file.path)
-        .resize(500)
-        .jpeg({ quality: 70 })
-        .toFile(
-            path.resolve(req.file.destination, 'resized', fileName) 
-        );
+        if (process.env.STORAGE_TYPE === 'local'){
 
-        fs.unlinkSync(req.file.path);
+            const [name] = image.split('.');
+            fileName = `${name}.jpg`;
+    
+            await sharp(req.file.path)
+            .resize(500)
+            .jpeg({ quality: 70 })
+            .toFile(
+                path.resolve(req.file.destination, 'resized', fileName) 
+            );
+    
+            fs.unlinkSync(req.file.path);
+
+        }
 
         const post = await Post.create({
             author,
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
-
+require('dotenv').config();
+const morgan = require('morgan');
 
 const app = express();
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-mongoose.connect("mongodb+srv://api-instagram:api-instagram@cluster0-6b2bu.mongodb.net/test?retryWrites=true&w=majority", {
+const mongoUrl = process.env.urlMongo;
+
+mongoose.connect(mongoUrl, {
     useNewUrlParser: true, // Configuracao para identificar o formato da conexao nova a partir de uma String.
     useUnifiedTopology: true
 });
@@ -23,8 +26,12 @@ app.use((req, resp, next) => {
 
 app.use(cors());
 
+app.use(express.json()); // Consegue lidar com corpo de mensagens vindos no formato JSON
+app.use(express.urlencoded({ extended: true })); // Consegue lidar com requisições no padrao URL Encoded
+app.use(morgan('dev')); // Lib de logs
+
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
-server.listen(8080);
+server.listen(3000);
